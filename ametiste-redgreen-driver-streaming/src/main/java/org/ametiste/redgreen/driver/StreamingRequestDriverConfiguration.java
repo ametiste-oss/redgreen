cebcfc77a4e9c1a625377c377d50e67e8b9a9d15
Konstantin A. Zolotukhin <masted@gmail.com>
2015-10-01 16:30:35 +0300

    some implemntation notices

--- ametiste-redgreen-driver-streaming/src/main/java/org/ametiste/redgreen/driver/StreamingRequestDriverConfiguration.java
+++ ametiste-redgreen-driver-streaming/src/main/java/org/ametiste/redgreen/driver/StreamingRequestDriverConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 public class StreamingRequestDriverConfiguration {
 
     // NOTE: redgreen core registering drivers using spring bean names
+    // NOTE: method name is overrided to provide ability to use constant name
+    // NOTE: via configuration
     @Bean(name=StreamingRequestDriverFactory.DRIVER_FACTORY_NAME)
     public StreamingRequestDriverFactory simpleStreamDriver() {
         return new StreamingRequestDriverFactory();
